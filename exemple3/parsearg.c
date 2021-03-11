3db3725f9cc020b7161b6668632540ac9432b9d4
zoeurk <zoeurk@gmail.com>
2021-03-11 23:02:16 +0100

    update

--- exemple3/parsearg.c
+++ exemple3/parsearg.c
@@ -56,7 +56,7 @@ void defaults(FILE *out, int *colonne, char shortoption, char *longoption, char 
 }
 void parser_usage(struct parser *parser){
 	struct parser_state state;
-	int colonne[2] = {ARG_COLONNE,EXPLAIN_COLONNE}, i, j, k, l, m, n, c = 0, c_;
+	int colonne[2] = {ARG_COLONNE,EXPLAIN_COLONNE}, i, j, k, l, m, n, z = 0, c = 0, c_;
 	char shortoption[3] = {'?', 0, 'V'},
 		*option[3] = {"help", "usage", "version"},
 		*explain[3] = {"show this message", "show short help", "show version"};
@@ -85,14 +85,18 @@ void parser_usage(struct parser *parser){
 		}
 	}
 	c++;
-	for(i = 0; options[i].longoption != 0; i++){
-		fprintf(state.out_stream," -%c, --", options[i].shortoption);
-		for(j = 0, k = 7; j < c && j < strlen(options[i].longoption);j++, k++){
+	/*n = 0;*/
+	for(i = 0;z = 0, options[i].longoption != 0; i++){
+		if(options[i].shortoption != 0)
+			fprintf(state.out_stream," -%c, --", options[i].shortoption);
+		else	fprintf(state.out_stream,"    --");
+		for(j = 0, k = 7,z = 0; j < c && j < strlen(options[i].longoption);j++, k++){
 			if(k < colonne[0])
 				fprintf(state.out_stream,"%c", options[i].longoption[j]);
 			else{
 				fprintf(state.out_stream,"\n %c", options[i].longoption[j]);
 				k = 0;
+				z = 1;
 			}
 		}
 		if(options[i].args){
@@ -120,7 +124,7 @@ void parser_usage(struct parser *parser){
 			m = 1;
 		}
 		if(k != 0)
-			for(k = k;k < colonne[0] + m +1; k++)
+			for(k = k+z;k < colonne[0] + m +1; k++)
 				fprintf(state.out_stream," "); 
 		for(k = 0, j = 0; j < strlen(options[i].explain); j++, k++){
 			if(k < colonne[1])
