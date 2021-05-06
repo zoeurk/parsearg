00fc111efab6d1367054b3da157bf9e1b101b4d1
zoeurk <zoeurk@gmail.com>
2021-05-06 23:13:10 +0200

    update

--- src/parsearg.c
+++ src/parsearg.c
@@ -49,7 +49,7 @@ void defaults(FILE *out, unsigned int *colonne, char shortoption, char *longopti
 		}
 	}
 	l = k-2;
-	*for(j = 0, k = 0; j < colonne[0]-l; j++, k++){
+	for(j = 0, k = 0; j < colonne[0]-l; j++, k++){
 		fprintf(out," ");
 	}
 	for(j = 0, k = 0; j < strlen(explain);j++, k++){
@@ -288,7 +288,7 @@ void parser_parse(struct parser *parser, int argc, char **argv, /*unsigned int f
 			state.explain_colonne = parser->state->explain_colonne;
 		else	state.explain_colonne = EXPLAIN_COLONNE;
 		if(parser->state->short_usage)
-			state.short_usage = parser->astate->short_usage;
+			state.short_usage = parser->state->short_usage;
 		else	state.short_usage = SHORT_USAGE;
 		/*memcpy(&state, parser->state, sizeof(struct parser_state));
 		if(parser->state->err_stream == NULL)
