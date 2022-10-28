fbab4d4de740e6dde0d907a6d7b47089155a5b2f
zoeurk <zoeurk@gmail.com>
2022-10-28 19:04:37 +0200

    update

--- exemple/prog.c
+++ exemple/prog.c
@@ -9,22 +9,19 @@ struct parser_option options[] =	{
 					{ "out", 'o', 0 , "STD", "test file2"},
 					{ "retest", 'r', 0, NULL, "grr"},
 					{ "retest2", '2', OPTION_ALIAS, NULL, "grr2"},
-					{ "putaincesttroplong", 'L', 0, NULL, "c'est vraiment tres long"},
+					{ "putaincesttroplong", 'L', 0, "euh", "c'est vraiment tres long"},
 					{ "option-totalement-null", 1, 0, "cool", "cette option est null"},
-					{ "option-de-merde-alors-putain", 2, 0,NULL, "putain mais la c'est vraiment la merde"},
+					{ "option-de-merde-alors-putain", 2, 0,"yes", "putain mais la c'est vraiment la merde"},
 					{0}
 				};
 struct arguments{
-	int i;
-	int pad;
 	char *buffer;
 };
 void arguments(int key, char *arg, struct parser_state *state){
 	struct arguments *arguments = (struct arguments *)state->input;
-	arguments->buffer = arg;
 	switch(key){
 		case 't':
-			printf("ok:%s\n",arg);
+			printf("test:%s\n",arg);
 			break;
 		case 'o': printf("out\n");
 			break;
@@ -33,31 +30,34 @@ void arguments(int key, char *arg, struct parser_state *state){
 			printf("retest\n");
 			break;
 		case 'L':
-			printf("putaincesttroplong\n");
+			printf("putaincesttroplong:%s\n", arg);
 			break;
-		case 1:printf("ok:%s\n", arg);
+		case 1:printf("option-totalement-null:%s\n", arg);
 			break;
-		case 2:printf("option-de-merde-alors-putain\n");
+		case 2:printf("option-de-merde-alors-putain:%s\n", arg);
 			break;
 		default:
-			printf("END:%s\n", arg);
+			if(arguments->buffer == NULL && arg != NULL){
+				if((arguments->buffer = calloc(strlen(arg)+1, sizeof(char))) == NULL){
+					perror("calloc()");
+					exit(EXIT_FAILURE);
+				}
+				strcpy(arguments->buffer,arg);
+			}else{
+				if(arg == NULL){
+					fprintf(stderr,"Erreur: argument manquant\n");
+					exit(EXIT_FAILURE);
+				}
+				if(arguments->buffer != NULL && arg != NULL){
+					fprintf(stderr,"Erreur: trop d'argmument:%s\n",arg);
+					exit(EXIT_FAILURE);
+				}
+			}
 	}
-	/*if(arg){
-		arguments->buffer = arg;
-		printf("[=>ok:%i:%c:arg:%s\n",key, key, arg);
-	}else{
-		arguments->buffer = NULL;
-		printf("[+>ok:%i:%c:no arg\n",key, key);
-	}*/
-	/*return 0;*/
 }
 struct parser args = {options, arguments, "[OPTIONS]", "string", "Exemple de programme", &program, NULL};
-/*void bye(void){
-	if(args.state)
-		free(args.state);
-}*/
 int main(int argc, char **argv){
-	struct arguments _args_ = {0, 0, NULL};
+	struct arguments _args_ = {NULL};
 	/*atexit(bye);*/
 	parser_parse(&args, argc, argv, &_args_);
 	/*if(_args_.buffer == NULL){
@@ -65,9 +65,13 @@ int main(int argc, char **argv){
 		fprintf(stderr, "Try %s -?|--usage\n",argv[0]);
 		exit(EXIT_FAILURE);
 	}*/
-	/*if(_args_.buffer)
-		printf("arguments: %s\n", _args_.buffer);*/
-	if((args.state = calloc(1,sizeof(struct parser_state))) == NULL){
+	if(_args_.buffer != NULL){
+		printf("arguments: %s\n", _args_.buffer);
+		free(_args_.buffer);
+	}else{ fprintf(stderr, "Erreur: argument  manquant\n"),
+			exit(EXIT_FAILURE);
+	}
+	/*if((args.state = calloc(1,sizeof(struct parser_state))) == NULL){
 		perror("calloc()");
 		exit(EXIT_FAILURE);
 	}
@@ -79,6 +83,6 @@ int main(int argc, char **argv){
 	args.state->arg_colonne = 35;
 	parser_usage(&args);
 	if(args.state)
-		free(args.state);
+		free(args.state);*/
 	return 0;
 }
